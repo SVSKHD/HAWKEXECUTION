dcd49435099760bdd24653d249c0b48b82721527
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2024-12-29 19:27:51 +0530

    enable symbols.

--- config.py
+++ config.py
@@ -1,32 +1,32 @@
 symbols_list = [
+    {
+        "symbol":"EURUSD",
+        "lot": 5.0,
+        "pip": 0.0001,
+        "threshold":15
+    },
     # {
-    #     "symbol":"EURUSD",
-    #     "lot": 5.0,
-    #     "pip": 0.0001,
-    #     "threshold":15
+    #     "symbol": "BTCUSD",
+    #     "lot": 1.0,
+    #     "pip": 1,
+    #     "threshold":2000
     # },
-    {
-        "symbol": "BTCUSD",
-        "lot": 1.0,
-        "pip": 1,
-        "threshold":2000
-    },
-    {
-        "symbol": "ETHUSD",
-        "lot": 1.0,
-        "pip": 1,
-        "threshold": 25
-    },
+    # {
+    #     "symbol": "ETHUSD",
+    #     "lot": 1.0,
+    #     "pip": 1,
+    #     "threshold": 25
+    # },
     # {
     #     "symbol": "BTCUSD",
     #     "lot": 1.0,
     #     "pip": 1,
     #     "threshold": 2000
     # }
-    # {
-    #     "symbol": "GBPUSD",
-    #     "lot": 5.0,
-    #     "pip": 0.0001,
-    #     "threshold":15
-    # },
+    {
+        "symbol": "GBPUSD",
+        "lot": 5.0,
+        "pip": 0.0001,
+        "threshold":15
+    },
 ]
